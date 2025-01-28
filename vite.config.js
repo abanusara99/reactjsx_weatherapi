6666a0ca44129ff0336c2f7dd9e94707f304fa93
Anupriya <96963618+abanusara99@users.noreply.github.com>
2025-01-28 17:02:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3001,  // Use Render's dynamic port or default to 3000 locally
+    host: '0.0.0.0',  // Listen on all network interfaces
+  },
 })
